6a2b2123d199da3ba17dc681b5b2c5c655ddfa6b
csc-nishida <nishida-yoichi-t@keyware.co.jp>
2023-03-02 10:21:21 +0900

    minor罫線調整

--- esnext/umd/vis-timeline-graph2d.js
+++ esnext/umd/vis-timeline-graph2d.js
@@ -5,7 +5,7 @@
  * Create a fully customizable, interactive timeline with items and ranges.
  *
  * @version 0.0.0-no-version
- * @date    2023-03-02T01:04:56.028Z
+ * @date    2023-03-02T01:20:07.447Z
  *
  * @copyright (c) 2011-2017 Almende B.V, http://almende.com
  * @copyright (c) 2017-2019 visjs contributors, https://github.com/visjs
@@ -3181,7 +3181,7 @@
       // let y = (orientation == 'top') ? props.majorLabelHeight + props.middleLabelHeight + props.minorLabelHeight: 
       // offsetを加算(どうなる？)
       // let y = (orientation == 'top') ? props.majorLabelHeight + props.middleLabelHeight + props.minorLabelHeight: 
-      let y = (orientation == 'top') ? props.majorLabelHeight + props.middleLabelHeight + props.minorLabelHeight + 7: 
+      let y = (orientation == 'top') ? props.majorLabelHeight + props.middleLabelHeight + props.minorLabelHeight: 
         this.body.domProps.top.height + props.majorLabelHeight + props.middleLabelHeight + props.minorLabelHeight;
       let x = left - props.minorLineWidth / 2;
 
