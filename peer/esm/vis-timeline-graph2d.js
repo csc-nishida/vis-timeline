71ada48e21b0f8ed654db88093746dbf5e488c16
csc-nishida <nishida-yoichi-t@keyware.co.jp>
2023-03-03 17:37:04 +0900

    innerHTMLへの設定文字調整

--- peer/esm/vis-timeline-graph2d.js
+++ peer/esm/vis-timeline-graph2d.js
@@ -5,7 +5,7 @@
  * Create a fully customizable, interactive timeline with items and ranges.
  *
  * @version 0.0.0-no-version
- * @date    2023-03-03T08:05:35.472Z
+ * @date    2023-03-03T08:35:11.866Z
  *
  * @copyright (c) 2011-2017 Almende B.V, http://almende.com
  * @copyright (c) 2017-2019 visjs contributors, https://github.com/visjs
