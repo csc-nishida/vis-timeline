c61976df1c6e9f470d57bd214be328cf8fe5b990
csc-nishida <nishida-yoichi-t@keyware.co.jp>
2023-03-03 17:07:23 +0900

    middlLabelの空白文字を全角とする

--- esnext/umd/vis-timeline-graph2d.js
+++ esnext/umd/vis-timeline-graph2d.js
@@ -5,7 +5,7 @@
  * Create a fully customizable, interactive timeline with items and ranges.
  *
  * @version 0.0.0-no-version
- * @date    2023-03-03T07:51:53.962Z
+ * @date    2023-03-03T08:05:35.472Z
  *
  * @copyright (c) 2011-2017 Almende B.V, http://almende.com
  * @copyright (c) 2017-2019 visjs contributors, https://github.com/visjs
@@ -3084,7 +3084,7 @@
       
       if (!label) {
         // create new label
-        const content = document.createTextNode(' ');
+        const content = document.createTextNode('　');
         label = document.createElement('div');
         label.appendChild(content);
         this.dom.foreground.appendChild(label);
